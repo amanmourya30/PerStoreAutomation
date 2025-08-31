9b8773713395ffcc3dacdc4350115ca80b54196c
aman_xten <aman@xtenav.com>
2025-08-31 20:22:22 +0530

    postusing hashmap test added

---
+++ src/test/java/api/tests/diffWaysToCreatPostReqBody.java
@@ -0,0 +1,49 @@
+package api.tests;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import api.endpoints.StudentsRoutes;
+import io.restassured.response.Response;
+
+//start local server: json-server --watch db.json --port 3000
+
+public class diffWaysToCreatPostReqBody {
+
+	String createdStudentId;
+
+	@Test(priority = 1)
+	public void testUsingHashMap() {
+
+		Map<String, Object> data = new HashMap<>();
+
+		data.put("name", "Scott");
+		data.put("location", "France");
+		data.put("phone", "123456789");
+		String[] courseArr = { "C", "C++" };
+		data.put("courses", courseArr);
+
+		Response response = given().contentType("application/json").body(data).when().post(StudentsRoutes.post_url);
+		response.then().statusCode(201).log().all();
+
+		createdStudentId = response.jsonPath().getString("id");
+		System.out.println("Created student with ID: " + createdStudentId);
+
+	}
+
+	@Test(priority = 2, dependsOnMethods = "testUsingHashMap")
+	public void testDeleteStudent() {
+		given().pathParam("id", createdStudentId).when().delete(StudentsRoutes.delete_url).then().statusCode(200).log()
+				.all();
+	}
+
+	@BeforeMethod
+	public void delay() throws InterruptedException {
+		Thread.sleep(500);
+	}
+}
